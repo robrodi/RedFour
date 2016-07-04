5b9fc0ad83ad3c91c710139b2e6097b74014d977
Rob Rodi <robert.rodi@gmail.com>
2016-07-04 15:01:10 -0700

    tests work

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -1,8 +1,28 @@
 defmodule PhysicsTest do
-  use ExUnit.Case
+use ExUnit.Case
   doctest Physics
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "nearest tenth rounds up" do
+    assert Converter.to_nearest_tenth(3.222) == 3.3
+  end
+
+  test "to_km" do
+    assert Converter.to_km(12345) == 12.345
+  end
+
+  test "round down" do
+    assert Converter.round_down(12.345) == 12
+  end
+
+  test "Ev is correct" do
+  	ev = Physics.Rocketry.escape_velocity(:earth)
+  	assert ev == 11.2
+
+  end
+
+  test "Ev is correct for another planet" do
+  	ev = %{mass: 4.0e22, radius: 6.21e6}
+  		|> Physics.Rocketry.escape_velocity
+  	assert ev == 1.0
   end
 end
