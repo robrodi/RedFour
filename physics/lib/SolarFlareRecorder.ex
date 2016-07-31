cde2567a7198a876de2115ff435a42d5f2ba2991
Rob Rodi <robert.rodi@gmail.com>
2016-07-31 09:33:47 -0700

    Switched SolarFlareRecorder to Agent. 2:26 in video

--- physics/lib/SolarFlareRecorder.ex
+++ physics/lib/SolarFlareRecorder.ex
@@ -2,15 +2,19 @@ defmodule SolarFlareRecorder do
 	use GenServer
 
 	def start_link do
-		GenServer.start_link __MODULE__, []
+		Agent.start_link  fn -> [] end
 	end	
 
 	def record_flare(pid, {:flare, classification: c, scale: s} = flare) do
-		GenServer.call(pid, {:record, flare})
+		Agent.get_and_update pid, fn(flares) ->
+			new_state = List.insert_at flares, -1, flare
+			{:ok, new_state}
+		end
 	end
 
-	def handle_call({:record, flare}, _sender, flares) do
-		flares = List.insert_at flares, -1, flare
-		{:reply, :ok, flares}
+	def get_flares(pid) do
+		Agent.get pid, fn(flares) ->
+			flares
+		end
 	end
 end
